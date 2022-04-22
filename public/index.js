23231c6fadd834a12af7cdeec0d9fa49fd6069bf
Martin <nitromagix@gmail.com>
2022-04-21 21:29:13 -0700

    finished assignment

--- public/index.js
+++ public/index.js
@@ -1,22 +1,36 @@
-async function main(){
+
+const update = async (counterValue) => {
+   const response = await fetch('http://localhost:9001/counter', {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        'value': counterValue
+      })
+      });
+
+   const j = await response.json();
+   console.log(j)
+}
+
+const main = async () => {
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
     const response = await fetch('http://localhost:9001/counter');
-
     const result = await response.json();
-    
     let countValue = result.value;
 
-    function increment(){
+    async function increment(){
         countValue++;
         countContainer.textContent = countValue;
+        await update(countValue);
     }
 
-    function decrement(){
+    async function decrement(){
         countValue--;
         countContainer.textContent = countValue;
+        await update(countValue);
     }
 
     incrementButton.addEventListener('click', increment);
